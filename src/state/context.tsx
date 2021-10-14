8dbbf963113b59805694c98275ca981ed22d54be
Nils <nils.goksor@gmail.com>
2021-10-14 22:30:10 +0100

    feat: keep user logged in with localstorage

--- src/state/context.tsx
+++ src/state/context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, ReactNode } from "react";
+import React, { createContext, useReducer, ReactNode, useEffect } from "react";
 import { AppReducer, StateI } from ".";
 
 export const initialState: StateI = {
@@ -20,7 +20,16 @@ interface AppProviderI {
 }
 
 const AppProvider: React.FC = ({ children }: AppProviderI) => {
-  const [state, dispatch] = useReducer(AppReducer, initialState);
+  const localState = JSON.parse(localStorage.getItem("forum-romanum") || "{}");
+  const reducerState =
+    Object.keys(localState).length > 0 ? localState : initialState;
+
+  const [state, dispatch] = useReducer(AppReducer, reducerState);
+
+  useEffect(() => {
+    localStorage.setItem("forum-romanum", JSON.stringify(state));
+  }, [state]);
+
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       {children}
